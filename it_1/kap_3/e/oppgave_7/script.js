04c8ffa8215826867315c2c6ad6d3c413c871301
OSTERITE <adrian@bandidos.org>
2023-03-01 10:41:41 +0100

    oppgaver 3e

--- it_1/kap_3/e/oppgave_7/script.js
+++ it_1/kap_3/e/oppgave_7/script.js
@@ -8,14 +8,12 @@ for (let i = 0; i < 6; i++) {
 console.log(random_array)
 
 function random_integer_in_range(lower_limit, upper_limit) {
-    return Math.floor(Math.random() * (upper_limit - lower_limit) + lower_limit);
+    return Math.floor(Math.random() * (upper_limit - lower_limit) + lower_limit );
 }
 
 function subtract_array(main_array, subtracting_array){
     for (let i = 0; i < subtracting_array.length; i++) {
-
         subtract_index = main_array.indexOf(subtracting_array[i])
-
         main_array.splice(subtract_index, 1)
     }
     return main_array
