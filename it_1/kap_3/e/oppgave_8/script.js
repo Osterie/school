32082aa1ba52138e3f6b6b1ed792edba7cf888b7
OSTERITE <adrian@bandidos.org>
2023-03-09 13:20:00 +0100

    ny zip fil og fjernet whitespace fra scripts

--- it_1/kap_3/e/oppgave_8/script.js
+++ it_1/kap_3/e/oppgave_8/script.js
@@ -1,13 +1,9 @@
-two_dimensional_array = []
+const two_dimensional_array = []
 
 for (let i = 0; i < 3; i++) {
-
     two_dimensional_array.push([])
-
     for (let j = 0; j < 3; j++) {
-
         two_dimensional_array[i][j] = j
-        
     }
 }
 
--- it_1/kap_3/e/oppgave_9/script.js
+++ it_1/kap_3/e/oppgave_9/script.js
@@ -1,9 +1,7 @@
-times_table = []
-
-for (let i = 0; i < 11; i++) {
+const times_table = []
+    for (let i = 0; i < 11; i++) {
     times_table.push([])
     for (let j = 0; j < 11; j++) {
-        times_table[i][j] = i*j
-    }
+        times_table[i][j] = i*j    }
 }
 console.log(times_table[8][7])
